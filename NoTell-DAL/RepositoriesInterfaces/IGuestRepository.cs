339d57e8eec049d9772a224fbbf33bd8fc6dc3a9
Michal Nawrocki <v25@vault25.net>
2021-04-22 23:41:34 +0200

    add repositories interfaces

---
+++ NoTell-DAL/RepositoriesInterfaces/IGuestRepository.cs
@@ -0,0 +1,10 @@
+﻿using NoTell_DAL.Entities;
+
+namespace NoTell_DAL.RepositoriesInterfaces
+{
+    public interface IGuestRepository
+    {
+        int AddGuest(string name, string lastName, string phone);
+        Guest GetGuestById(int guestId);
+    }
+}
---
+++ NoTell-DAL/RepositoriesInterfaces/IReservationRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using NoTell_DAL.Entities;
+
+namespace NoTell_DAL.RepositoriesInterfaces
+{
+    public interface IReservationRepository
+    {
+        int AddReservation(DateTime reservationFrom, DateTime reservationTo, int guestId, int roomId);
+        void DeleteReservation(int reservationId);
+        void UpdateReservation(int reservationId, DateTime reservationFrom, DateTime reservationTo, int guestId,
+                               int roomId);
+        Reservation GetReservationById(int reservationId);
+        IEnumerable<Reservation> GetReservationList();
+    }
+}
