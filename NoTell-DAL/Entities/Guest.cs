d06bb7b61ad06c65f53558f4d1060eb691a55b58
Michal Nawrocki <v25@vault25.net>
2021-04-22 23:41:13 +0200

    add entities

---
+++ NoTell-DAL/Entities/Guest.cs
@@ -0,0 +1,10 @@
+﻿namespace NoTell_DAL.Entities
+{
+    public class Guest
+    {
+        public int GuestId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
