3cfeb93f5f6876e97c8f3665bb8ce135a76be176
Michal Nawrocki <v25@vault25.net>
2021-04-23 10:49:30 +0200

    add view models

---
+++ NoTell-Services/ViewModel/AddReservationVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoTell_Services.ViewModel
+{
+    public class AddReservationVM
+    {
+        public DateTime ReservationFrom { get; set; }
+        public DateTime ReservationTo { get; set; }
+        public int RoomId { get; set; }
+        public int GuestId { get; set; }
+    }
+}
---
+++ NoTell-Services/ViewModel/AvailableRoomsForDataRangeVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NoTell_Services.ViewModel
+{
+    public class AvailableRoomsForDataRangeVM
+    {
+        public DateTime ReservationFrom { get; set; }
+        public DateTime ReservationTo { get; set; }
+    }
+}
