2213aa9a64b0417b697c075009af98e23dd2ad83
Michal Nawrocki <v25@vault25.net>
2021-04-23 02:46:30 +0200

    add Guest View Models

---
+++ NoTell-API/ViewModel/AddGuestVM.cs
@@ -0,0 +1,9 @@
+﻿namespace NoTell_API.ViewModel
+{
+    public class AddGuestVM
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
